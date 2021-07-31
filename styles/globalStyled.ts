01ec28a46817e9ba1f271df7dc865f25c50b4ed9
Luis <luisdiaslima25@gmail.com>
2021-07-31 12:43:19 -0300

    update wish list styles

--- styles/globalStyled.ts
+++ styles/globalStyled.ts
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const MainContainer = styled.div`
     display: flex;
     align-items: flex-start;
-    justify-content: center;
+    justify-content: flex-start;
     flex-direction: column;
     padding: 35px;
 
